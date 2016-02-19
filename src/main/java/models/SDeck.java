f8dc8b4b5d2e8a7558ff08dc8d200310b5f4bf9a
Charles Henninger <Cthenninger@gmail.com>
2016-02-18 17:49:10 -0800

    Added the UDeck class's buildDeck() function. Also deleted the drop locations in the view file to avoid cards from sticking there when dragged.

--- src/main/java/models/SDeck.java
+++ src/main/java/models/SDeck.java
@@ -4,4 +4,14 @@ package models;
  * Created by paul on 2/17/16.
  */
 public class SDeck extends Deck {
+
+    public void buildDeck() {  //remove: just for testing Udeck
+        for(int i = 2; i < 15; i++){
+            deck.add(new Card(i,Suit.Clubs));
+            deck.add(new Card(i,Suit.Hearts));
+            deck.add(new Card(i,Suit.Diamonds));
+            deck.add(new Card(i,Suit.Spades));
+        }
+    }
 }
+
--- src/main/java/models/UDeck.java
+++ src/main/java/models/UDeck.java
@@ -4,4 +4,14 @@ package models;
  * Created by paul on 2/17/16.
  */
 public class UDeck extends Deck {
+
+    public void buildDeck() {
+        for(int i = 2; i < 15; i++){
+            deck.add(new Card(i,Suit.Clubs));
+            deck.add(new Card(i,Suit.Hearts));
+            deck.add(new Card(i,Suit.Diamonds));
+            deck.add(new Card(i,Suit.Spades));
+        }
+    }
+
 }
